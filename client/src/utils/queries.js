1711e7380fa3870579daddfb595d6b6c055185bd
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-08-31 21:09:30 -0700

    Created Me query

---
+++ client/src/utils/queries.js
@@ -0,0 +1,12 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+    query me($userId: ID!) {
+        user(userId: $userId) {
+            _id
+            username
+            bookCount
+            savedBooks
+        }
+    }
+`;
