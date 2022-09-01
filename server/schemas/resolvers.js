9b5109922160e2d59edbb3d29224f6c30cdca122
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-08-31 20:24:28 -0700

    Added query and createUser and login mutations

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,31 @@
+const { Book, User } = require("../models"); 
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query:  {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id });
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        }
+               
+    }, 
+
+    Mutation: {
+        createUser: async(parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+
+            return { token, user };
+        }, 
+        login: async (parent, { username, password }) => {
+            const user = await User.findOne({ username });
+
+            if (!user) {
+                throw new AuthenticationError('No profile with this email found!');
+            }
+        }
+    }
+}
