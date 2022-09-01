89f18da9cc0a9f9dbf338332dc1081259699d6c5
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-08-31 21:11:49 -0700

    Corrected ME query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,12 +1,23 @@
 import { gql } from '@apollo/client';
 
+// export const GET_ME = gql`
+//     query me($userId: ID!) {
+//         user(userId: $userId) {
+//             _id
+//             username
+//             bookCount
+//             savedBooks
+//         }
+//     }
+// `;
+
 export const GET_ME = gql`
-    query me($userId: ID!) {
-        user(userId: $userId) {
-            _id
-            username
-            bookCount
-            savedBooks
-        }
+  query me {
+    me {
+      _id
+      username
+      bookCount
+      savedBooks
     }
+  }
 `;
