c5e5481e55904777502ece84de8d9a0e562cae74
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-09-02 15:43:03 -0700

    Created login, adduser, savebook, and deletebook mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,63 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-    mutation login(){}
-`
+    mutation loginUser($username: String!, $password: String!){
+        login(username: $username, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`; 
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token 
+            user {
+                _id
+                username
+            }
+        }
+    }
+`; 
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($body: body!) {
+        saveBook(body: $body) {
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+            authors
+            image
+            description
+            title 
+            link
+        }
+    }
+}
+`; 
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username 
+            email
+            bookCount 
+            savedBooks {
+                bookId 
+                authors
+                description 
+                image
+                description 
+                title 
+                link
+            }
+        }
+    }
+`; 
