dea99618221390e062f6bdf892dd908e88593ed3
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-09-02 16:11:44 -0700

    Imported apollo hook and mutations

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import { useMutation } from "@apollo/client";
+import { SAVE_BOOK } from "../utils/mutations"; 
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
