8219293efa9dd9c086c1580e89c25da9cec3db13
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-09-03 15:49:31 -0700

    Changed some property names

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -76,8 +76,12 @@ const SearchBooks = () => {
       // }
 
       const { data } = await saveBook({
-        variables: { body: body }
+        variables: { input: bookToSave }
       }); 
+
+      if (error) {
+        throw new Error('something went wrong!');
+      }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
