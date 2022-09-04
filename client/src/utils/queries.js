e0825e8aaf8b520f8e29b48d8ca8ccb916703655
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-09-03 19:24:53 -0700

    Cleared out extraneous code, added more properties to query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,23 +1,19 @@
 import { gql } from '@apollo/client';
 
-// export const GET_ME = gql`
-//     query me($userId: ID!) {
-//         user(userId: $userId) {
-//             _id
-//             username
-//             bookCount
-//             savedBooks
-//         }
-//     }
-// `;
-
 export const GET_ME = gql`
   query me {
     me {
       _id
       username
       bookCount
-      savedBooks
+      savedBooks {
+        bookId
+        authors 
+        image 
+        description 
+        title 
+        link
+      }
     }
   }
 `;
