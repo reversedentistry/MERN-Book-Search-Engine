2a06b6c0bfccd2995752ef3b26bacbc3cd9422bd
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-09-03 19:22:13 -0700

    Cleaned up codee

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,42 +2,38 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
     type User {
-    _id: ID,
-    username: String,
-    email: String, 
-    password: String, 
-    bookCount: Int,
-    savedBooks: [String]
+    _id: ID
+    username: String
+    email: String 
+    bookCount: Int
+    savedBooks: [Book]
     } 
 
     type Book {
-      bookId: String,
-      authors: [String], 
-      description: String,
-      title: String,
-      image: String,
+      bookId: String
+      authors: [String]
+      description: String
+      title: String
+      image: String
       link: String
         
     }
 
     type Auth {
-    token: ID,
+    token: ID
     user: User
   }    
 
   type Query {
-    users: [User],
-    user(_id: String): User,
-    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
     me: User
   }
 
   input bookInput {
     bookId: String
-    authors: [String],
-    description: String,
-    title: String,
-    image: String,
+    authors: [String]
+    description: String
+    title: String
+    image: String
     link: String
   }
 
